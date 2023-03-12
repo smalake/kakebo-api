0ec0f38ce847a4ed5116c5a607e0313ef1272b29
smalake <41889150+smalake@users.noreply.github.com>
2023-03-12 17:48:23 +0900

    Firebaseを使ったJWT認証の実装

--- middleware/authCheck.go
+++ middleware/authCheck.go
@@ -1,61 +1,47 @@
 package middleware
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gorilla/sessions"
-	"github.com/smalake/kakebo-api/model"
+	firebase "firebase.google.com/go"
 	"github.com/smalake/kakebo-api/utils/logging"
+	"google.golang.org/api/option"
 )
-
-// セッション用のストア
-var store = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_KEY")))
 
 func AuthCheck(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenHeader := r.Header.Get("Authorization")
-		if tokenHeader == "" {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		// トークンの形式を検証
-		bearerToken := strings.Split(tokenHeader, " ")
-		if len(bearerToken) != 2 {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		// JWTの検証
-		token, err := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-			return []byte(os.Getenv("JWT_SECRET")), nil
-		})
+		// Firebase SDK のセットアップ
+		opt := option.WithCredentialsFile(os.Getenv("FIREBASE_CREDENTIALS_FILE"))
+		app, err := firebase.NewApp(context.Background(), nil, opt)
 		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
+			logging.WriteErrorLog(err.Error(), true)
+			os.Exit(1)
+		}
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			logging.WriteErrorLog(err.Error(), true)
+			os.Exit(1)
 		}
 
-		// JWTからメールアドレスを抽出
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			email := claims["email"].(string)
-			// 抽出したメールアドレスからユーザIDを取得
-			id, err := model.GetUserId(email)
-			if err != nil {
-				logging.WriteErrorLog(err.Error(), true)
-				return
-			}
-			// セッションを開始
-			session, _ := store.Get(r, os.Getenv("SESSION_NAME"))
-			session.Values["id"] = id //ユーザIDをセッションに保存
-			session.Save(r, w)
+		// クライアントから送られてきた JWT 取得
+		authHeader := r.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		// JWT の検証
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			// JWT が無効なら Handler に進まず別処理
+			fmt.Printf("error verifying ID token: %v\n", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("error verifying ID token\n"))
+			return
 		}
+		log.Printf("Verified ID token: %v\n", token)
 		next.ServeHTTP(w, r)
 	})
 }
