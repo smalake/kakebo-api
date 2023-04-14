1be02762f5ba3c55ec22bf99c899adcf8b70b3d4
smalake <41889150+smalake@users.noreply.github.com>
2023-04-14 12:13:25 +0900

    CORS対策

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"github.com/smalake/kakebo-api/server"
 	"github.com/smalake/kakebo-api/utils/firebase"
 	"github.com/smalake/kakebo-api/utils/logging"
@@ -24,9 +26,18 @@ func main() {
 	// ルーティングを呼び出す
 	server.Routing(r)
 
+	// CORS設定
+	corsOpts := cors.New(cors.Options{
+		AllowedOrigins:   []string{os.Getenv("FRONT_URL")},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	})
+	c := corsOpts.Handler(r)
+
 	// HTTPサーバーを起動する
 	log.Println("Starting server on :8088...")
-	if err := http.ListenAndServe(":8088", r); err != nil {
+	if err := http.ListenAndServe(":8088", c); err != nil {
 		logging.WriteErrorLog(err.Error(), true)
 		log.Fatal(err)
 	}
